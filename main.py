416355c1cd1cc0b6045ba3091d1ca4758ea3cfaf
Sari Fink <fink.sari@gmail.com>
2020-05-04 20:54:45 +0200

    update

--- main.py
+++ main.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for
+import random
+import datetime
 
 app = Flask(__name__)
 
-
 @app.route("/")
-def index():
-    return "Hello, SmartNinja!"
+def main():
+    return render_template("main.html")
+
+@app.route("/fakebook")
+def fakebook():
+    return render_template("fakebook.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/lucky_number", methods=['GET', 'POST'])
+def lucky_number():
+
+    if request.method == "GET":
+        context = {
+            "date_of_number": datetime.datetime.now().isoformat(),
+            "lucky_number": random.randint(1, 10)
+        }
+        return render_template("lucky_number.html", **context)
+    elif request.method == "POST":
+        return redirect(url_for('lucky_number'))
+
+
+# TODO1: add boogle site
+# TODO2: add hairdresser site
+
+# mein neuer code kommt hier
 
 
 if __name__ == '__main__':
