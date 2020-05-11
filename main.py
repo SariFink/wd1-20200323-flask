e5a8094810875907e65429897c8c9dc9670806dc
Sari Fink <FinkS@compax.intern>
2020-05-11 18:44:17 +0200

    Update 11052020

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, make_response
 import random
 import datetime
 
@@ -8,25 +8,46 @@ app = Flask(__name__)
 def main():
     return render_template("main.html")
 
+
 @app.route("/fakebook")
 def fakebook():
     return render_template("fakebook.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+
 @app.route("/lucky_number", methods=['GET', 'POST'])
 def lucky_number():
 
     if request.method == "GET":
+        secret_number = random.randint(1, 10)
+
         context = {
             "date_of_number": datetime.datetime.now().isoformat(),
-            "lucky_number": random.randint(1, 10)
+            "lucky_number": secret_number
         }
-        return render_template("lucky_number.html", **context)
+
+        response = make_response(render_template("lucky_number.html", **context))
+        response.set_cookie("lucky_number/secret", secret_number)
+
+        return
+
     elif request.method == "POST":
+        user_guess = request.form.get('number')
+        user_secret_cookie = request.cookies.get("lucky_number/secret")
+        if user_guess == user_secret_cookie:
+            print("User succeeded in guessing")
+            return
+        else:
+            print("user guessed wrong: ", user_guess, "but secret number was: ", user_secret_cookie)
         return redirect(url_for('lucky_number'))
+
+@app.route("/lucky_number/succeess", methods=["GET"])
+def lucky_number_success():
+    return render_template("lucky_number_success.html")
 
 
 # TODO1: add boogle site
